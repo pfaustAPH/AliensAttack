6078dba3ac6fcd93d41c6617e4a7ef8b07367a80
pfaustAPH <pfaust@aph.org>
2023-11-06 11:40:26 -0500

    Move aliens back and forth and down

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.update_bullets()
+            self._update_bullets()
+            self._update_aliens()
             # print(len(self.bullets))
             self._update_screen()
 
@@ -42,7 +43,7 @@ class AlienInvasion:
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
-    def update_bullets(self):
+    def _update_bullets(self):
         """ Update position of bullets and get rid of old bullets """
         # Update bullet positions
         self.bullets.update()
@@ -51,6 +52,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """ Check if the fleet is at an edge, then update the positions of all aliens in the fleet """
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         """ Respond to keypresses and mouse events """
@@ -114,6 +120,19 @@ class AlienInvasion:
             for alien_number in range(number_aliens_x):
                 # Create an alien and place it in a row
                 self._create_alien(alien_number, row_number)
+
+    def _check_fleet_edges(self):
+        """ Respond appropriately if any aliens have reached an edge """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet's direction """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _create_alien(self, alien_number, row_number):
         """ Create an alien and place it in the row """
--- settings.py
+++ settings.py
@@ -13,3 +13,7 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents let
+        self.fleet_direction = 1
