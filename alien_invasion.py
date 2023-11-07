982caab67e75eefdaa3894c88015124b2783d21c
pfaustAPH <pfaust@aph.org>
2023-11-06 20:42:44 -0500

    has errors

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -24,6 +25,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
 
+        # Make the Play button
+        self.play_button = Button(self, "Play")
+
     def run_game(self):
         """ Start the main loop for the game """
         while True:
@@ -34,9 +38,6 @@ class AlienInvasion:
                 self._update_aliens()
                 # print(len(self.bullets))
                 self._update_screen()
-            else:
-                print("Game Over!")
-                break
 
     def _update_screen(self):
         """ Update images on the screen and flip to the new screen """
@@ -46,6 +47,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
