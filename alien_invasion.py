efd4a737cfa4535f092de92754500cdeb2eefb4a
pfaustAPH <pfaust@aph.org>
2023-11-07 10:14:21 -0500

    Working Play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,15 +14,19 @@ class AlienInvasion:
         """ Initialize the game, and create game resources """
         pygame.init()
         self.settings = Settings()
-        #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        
+        #Create an instance to store game statistics
         self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
+
         self._create_fleet()
 
         # Make the Play button
@@ -37,7 +41,7 @@ class AlienInvasion:
                 self._update_bullets()
                 self._update_aliens()
                 # print(len(self.bullets))
-                self._update_screen()
+            self._update_screen()
 
     def _update_screen(self):
         """ Update images on the screen and flip to the new screen """
